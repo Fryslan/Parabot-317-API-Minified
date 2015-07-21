7a17425e7da50190fa58b554f51ea59e3f4b7aae
Fryslan <pietjetse@outlook.com>
2015-07-21 14:20:51 +0200

    Use Dijkstra Algorithm for distance check?
    Did without Idea but should be fine i guess.

--- src/org/rev317/min/api/methods/Calculations.java
+++ src/org/rev317/min/api/methods/Calculations.java
@@ -27,9 +27,7 @@ public class Calculations {
      * @return distance between a and b
      */
     public static final double distanceBetween(Tile a, Tile b) {
-        int x = b.getX() - a.getX();
-        int y = b.getY() - a.getY();
-        return Math.sqrt((x * x) + (y * y));
+        return dijkstraDist(a.getX(),a.getY(),b.getX,b.getY());
     }
 
 
